1664c8825147a3c9a24d0ca732a030213d80f3c5
Robin Claesson <57729281+RobinClaesson@users.noreply.github.com>
2026-03-01 11:29:34 +0100

    Add Markdown editor

--- Yarm.Wasm/Pages/AddRecipe/AddRecipePage.razor.cs
+++ Yarm.Wasm/Pages/AddRecipe/AddRecipePage.razor.cs
@@ -1,9 +1,9 @@
 using Fluxor;
 using Fluxor.Blazor.Web.Components;
 using Microsoft.AspNetCore.Components;
-using Yarm.Common.Models;
+using PSC.Blazor.Components.MarkdownEditor;
+using Yarm.Wasm.Services;
 using Yarm.Wasm.Store.Edit;
-using Yarm.Wasm.Store.Recipe;
 
 namespace Yarm.Wasm.Pages.AddRecipe;
 
@@ -11,14 +11,11 @@ public partial class AddRecipePage : FluxorComponent
 {
     [Inject] private IState<EditRecipeState> EditRecipeState { get; set; } = null!;
     [Inject] private IDispatcher Dispatcher { get; set; } = null!;
-    
+    [Inject] private IRecipeSourcesService RecipeSourcesService { get; set; } = null!;
+
     private Task OnMarkdownValueChanged(string value)
     {
         Dispatcher.Dispatch(new EditRecipeContentUpdatedAction(value));
         return Task.CompletedTask;
-    }
-
-    private void AddRecipeStep()
-    {
     }
 }
